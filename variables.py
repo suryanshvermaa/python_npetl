ecebac2b97d426d5cbc124f9be6734303267d1f2
Suryansh Verma <suryanshv.ug23.ee@nit.ac.in>
2025-04-27 14:55:12 +0530

    variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+name="Suryansh" #string
+age=20 #integer
+weight=68.5 #float
+isAdult=True #boolean
+print(name,age,weight)
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(isAdult))
+
+#multiline comment
+"""
+This is multiline comment
+"""
+
+#type casting
+a,b=1,"2"
+c=int(b)
+sum=a+c
+print(sum)
